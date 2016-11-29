28721a6ef665fc410d44304416052f60b655ef10
zermo <zermo95@hotmail.it>
2016-11-29 16:06:05 +0100

    implementazione fino a task Support different delimiters

--- src/it/uniba/tdd/StringCalculator.java
+++ src/it/uniba/tdd/StringCalculator.java
@@ -1,9 +1,47 @@
 package it.uniba.tdd;
 
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
-		// not yet implemented
-		return 0;
+
+	public int add(String numbersStr) throws StringCalculatorException {
+		// caso base
+		if (numbersStr.isEmpty()) {
+			return 0;
+		}
+
+		// caso in cui c'è un solo numero
+		if (numbersStr.length() == 1) {
+			return Integer.parseInt(numbersStr);
+		}
+
+		// caso in cui ci sono più numeri
+		else {
+			int totalSum = 0;
+			String delimiter = ",";
+
+			// caso in cui si vuole impostare un altro delimitatore
+			// se non viene impostato alcun delimitatore
+			// rimane di default la virgola
+			if (numbersStr.contains("//")) {
+				delimiter = numbersStr.substring(2, 3);
+				//System.out.println(delimiter);
+				
+				//elimina i tre caratteri iniziali
+				numbersStr = numbersStr.substring(4);
+				//System.out.println(numbersStr);
+			}
+
+			String[] strArray = numbersStr.split("(" + delimiter + ")" + "|(\n)");
+			
+			if (numbersStr.contains(",\n") || numbersStr.contains("\n,") || numbersStr.contains(delimiter + "\n")) {
+				throw new StringCalculatorException();
+			}
+			int[] intArray = new int[strArray.length];
+			for (int i = 0; i < strArray.length; i++) {
+				intArray[i] = Integer.parseInt(strArray[i]);
+				totalSum += intArray[i];
+			}
+			return totalSum;
+
+		}
 	}
 }
--- src/it/uniba/tdd/tests/StringCalculatorTest.java
+++ src/it/uniba/tdd/tests/StringCalculatorTest.java
@@ -2,13 +2,69 @@ package it.uniba.tdd.tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import it.uniba.tdd.StringCalculator;
+import it.uniba.tdd.StringCalculatorException;
+
 public class StringCalculatorTest {
+	
+	StringCalculator stringCalculator;
+	
+
+	@Before
+	public void initialize() {
+		stringCalculator = new StringCalculator();
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void addEmptyString() throws StringCalculatorException {
+		int result = stringCalculator.add("");
+		assertEquals(0, result);
 	}
+	
+	@Test
+	public void addOneCharacterString() throws StringCalculatorException{
+		int result = stringCalculator.add("1");
+		assertEquals(1, result);
+	}
+	
+	@Test
+	public void addTwoCharacterString() throws StringCalculatorException{
+		int result = stringCalculator.add("1,2");
+		assertEquals(3, result);
+	}
+	
+	@Test
+	public void addUnknowCharacterString() throws StringCalculatorException{
+		int result = stringCalculator.add("1,2,3");
+		assertEquals(6, result);
+	}
+	
+	@Test
+	public void addUnknowWithNewLineString() throws StringCalculatorException{
+		int result = stringCalculator.add("1\n2,3");
+		assertEquals(6, result);
+	}
+	
+	@Test (expected = StringCalculatorException.class)
+	public void addUnknowWithNewLineFinalString() throws StringCalculatorException{
+		stringCalculator.add("1,\n");
+		stringCalculator.add("1\n,");
+	}
+	
+	
+	@Test
+	public void addCharacterWithNoDefaultDelimiter() throws StringCalculatorException{
+		int result = stringCalculator.add("//;\n1;2");
+		assertEquals(3, result);
+	}
+	
+	
+	
+	
+	
+	
 
 }
